47057703437fe9d12b5efc9efd7cd5b103b22ea9
teemal <taymal1987@gmail.com>
2019-11-17 16:02:37 -0800

    finished command line args and exercises

--- command_line_args/command_line_args.go
+++ command_line_args/command_line_args.go
@@ -3,13 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
+	for i := 0; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+
+	t := ""
+	//Go doesn't allow unused variables
+	//since we are getting an index and index value from range os.Args
+	//we can just pass the index into a blank identifier(_) since it won't be used
+	for i, arg := range os.Args[1:] {
+		t = " index: " + strconv.Itoa(i)
+		fmt.Println(arg + t)
+	}
+
+	//less constly version of echo
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
