6b6bb5403f69391479bd97d28d5c035a33f11a80
teemal <taymal1987@gmail.com>
2019-11-18 20:01:50 -0800

    finished 1.3

--- 1.3_duplicating_lines/duplicating_lines.go
+++ 1.3_duplicating_lines/duplicating_lines.go
@@ -1,21 +1,21 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
 
-func main() {
-	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
+// func main() {
+// 	counts := make(map[string]int)
+// 	input := bufio.NewScanner(os.Stdin)
 
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
-}
+// 	for input.Scan() {
+// 		counts[input.Text()]++
+// 	}
+// 	for line, n := range counts {
+// 		if n > 1 {
+// 			fmt.Printf("%d\t%s\n", n, line)
+// 		}
+// 	}
+// }
