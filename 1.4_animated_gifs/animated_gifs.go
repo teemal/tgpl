958aa02fdf254d873aa632af1fd9c99121d8cb11
teemal <taymal1987@gmail.com>
2019-11-19 17:55:27 -0800

    finished 1.4

---
+++ 1.4_animated_gifs/animated_gifs.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{0, 255, 0, 255}}
+
+const (
+	blackIndex  = 2
+	greenIndex  = 1
+	orangeIndex = 0
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles = 5      //number of complete x oscillator revs
+		res    = 0.0001 //angular resolution
+		size   = 100    //image canvas covers [-size..+size]
+		nframe = 64     //animation frames
+		delay  = 8      //delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 //relative freq of y oscillator
+	anim := gif.GIF{LoopCount: nframe}
+	phase := 0.0 //phase difference
+	for i := 0; i < nframe; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) //ignoring encoding errors
+}
