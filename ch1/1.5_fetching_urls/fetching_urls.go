4f2863a2d2c843c65b89e893076a85913b26a804
teemal <taymal1987@gmail.com>
2019-11-20 17:41:22 -0800

    finished ex 1.8

--- ch1/1.5_fetching_urls/fetching_urls.go
+++ ch1/1.5_fetching_urls/fetching_urls.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,7 +26,24 @@ func main() {
 	// }
 
 	//exercise 1.7
+	// for _, url := range os.Args[1:] {
+	// 	resp, err := http.Get(url)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+	// 		os.Exit(1)
+	// 	}
+	// 	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+	// 		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+	// 		os.Exit(1)
+	// 	}
+	// 	resp.Body.Close()
+	// }
+
+	//exercise 1.8
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -37,6 +55,4 @@ func main() {
 		}
 		resp.Body.Close()
 	}
-
-	//exercise 1.8
 }
