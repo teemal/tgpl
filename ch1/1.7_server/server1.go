8ae2bf2da7a508d3b1648a818c17c11ba603d1e4
teemal <taymal1987@gmail.com>
2019-11-25 19:14:37 -0800

    finished server 3

--- ch1/1.7_server/server1.go
+++ ch1/1.7_server/server1.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 )
 
 //first server
@@ -18,25 +17,47 @@ import (
 // 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 // }
 
-var mu sync.Mutex
-var count int
+//2ns server
+// var mu sync.Mutex
+// var count int
+
+// func main() {
+// 	http.HandleFunc("/", handler) //each request calls handler
+// 	http.HandleFunc("/count", counter)
+// 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+// }
+
+// //handler echoes the path of the request URL
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	mu.Lock()
+// 	count++
+// 	mu.Unlock()
+// 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+// }
+
+// func counter(w http.ResponseWriter, r *http.Request) {
+// 	mu.Lock()
+// 	fmt.Fprintf(w, "Count %d\n", count)
+// 	mu.Unlock()
+// }
 
 func main() {
 	http.HandleFunc("/", handler) //each request calls handler
-	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 //handler echoes the path of the request URL
 func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-}
-
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
+	fmt.Fprintf(w, "%s %s %s", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "Remote address = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q = %q", k, v)
+	}
 }
