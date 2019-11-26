325fb630a1ca098cf1be6a32d95db59f3f199578
teemal <taymal1987@gmail.com>
2019-11-25 19:03:48 -0800

    finished 2nd server

--- ch1/1.7_server/server1.go
+++ ch1/1.7_server/server1.go
@@ -4,14 +4,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+//first server
+// func main() {
+// 	http.HandleFunc("/", handler) //each request calls handler
+// 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+// }
+
+// //handler echoes the path of the request URL
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+// }
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler) //each request calls handler
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 //handler echoes the path of the request URL
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
 }
